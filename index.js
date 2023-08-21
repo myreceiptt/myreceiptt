43466ead32724b278d18188ece39ac5576b84268
myreceiptt <nota@straight-line.org>
2023-08-21 07:04:31 +0700

    react

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ root.render(
   </StrictMode>
 );
 
-{/* <>
+{
+  /* <>
 <div>
 <h4>DevOps: Initialization Page!</h4>
 </div>
@@ -19,4 +20,5 @@ root.render(
 <p>Please visit this link:<a href="https://deeplinks.straight-line.org/" target="_blank" rel="noreferrer">Prof. NOTA's Deep Links</a></p>
 <p><i>Please note that this link merely provide list of <a href="https://deeplinks.straight-line.org/" target="_blank" rel="noreferrer">Prof. NOTA's Deep Links</a> and does not facilitate the access to or use of the Deep Links.</i></p>
 </div>
-</> */}
+</> */
+}

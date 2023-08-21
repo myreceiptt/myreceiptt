4936413a2bd042578acad26efaed7a4708837cb6
myreceiptt <nota@straight-line.org>
2023-08-21 07:02:53 +0700

    react

---
+++ index.js
@@ -0,0 +1,22 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+{/* <>
+<div>
+<h4>DevOps: Initialization Page!</h4>
+</div>
+<div>
+<p>Please visit this link:<a href="https://deeplinks.straight-line.org/" target="_blank" rel="noreferrer">Prof. NOTA's Deep Links</a></p>
+<p><i>Please note that this link merely provide list of <a href="https://deeplinks.straight-line.org/" target="_blank" rel="noreferrer">Prof. NOTA's Deep Links</a> and does not facilitate the access to or use of the Deep Links.</i></p>
+</div>
+</> */}

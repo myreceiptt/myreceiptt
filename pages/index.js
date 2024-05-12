6fdeef7672ef24617e5e87a52b85735a2d408d47
Prof. NOTA <141787406+myreceiptt@users.noreply.github.com>
2024-05-13 01:48:36 +0700

    index.js

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ export default function Home() {
         <link rel="icon" href="/1.47.png" />
         <link rel="apple-touch-icon" href="/1.47.png" />
         <link rel="shortcut icon" href="/1.47.png" />
+        <link rel="canonical" href="https://init.straight-line.org/" />
+
         <meta
           name="description"
           content="The init page initializes and controls processes. Its primary role is to start processes based on records read from the MyReceipt files. MyReceipt files usually requests that the init command run the getty command for each line on which process can log in."
